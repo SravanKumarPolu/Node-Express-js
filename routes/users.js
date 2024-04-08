8a7410cf4a8ef6767b80723de8aa5306238b9148
PVSKR <skr.polu@hotmail.com>
2024-04-08 20:48:32 +0530

    middleware use

--- routes/users.js
+++ routes/users.js
@@ -7,5 +7,26 @@ router.get("/", (req, res) => {
 router.get("/new", (req, res) => {
   res.send("new user form");
 });
+router.post("/", (req, res) => {
+  res.send("create user");
+});
 
+router
+  .route("/:id")
+  .get((req, res) => {
+    console.log(req.user); // accessing req.user instead of res.user
+    res.send(`Get user with id ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Update user with id ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Delete user with id ${req.params.id}`);
+  });
+
+const users = [{ name: "sravan" }, { name: "kumar" }];
+router.param("id", (req, res, next, id) => {
+  req.user = users[id];
+  next();
+});
 module.exports = router;
