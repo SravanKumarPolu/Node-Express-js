44a1d01b087fe891b4df9c493d44928d6d1d8388
PVSKR <skr.polu@hotmail.com>
2024-04-08 19:39:57 +0530

    express js how to render html and server.

--- server.js
+++ server.js
@@ -13,17 +13,33 @@
 // });
 
 // middleware
+// const express = require("express");
+// const app = express();
+
+// app.use((req, res, next) => {
+//   console.log(`${req.methods} request recevied for ${req.url}`);
+//   next();
+// });
+
+// // Route handling logic
+// // ...
+
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
+
 const express = require("express");
+
 const app = express();
 
-app.use((req, res, next) => {
-  console.log(`${req.methods} request recevied for ${req.url}`);
-  next();
-});
+app.set("view engine", "ejs");
 
-// Route handling logic
-// ...
+app.get("/", (req, res) => {
+  console.log("Here");
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  //   res.status(500).send("Hit");
+  //   res.status(500).json({ message: "err " });
+  //   res.download("server.js");
+  res.render("index", { text: "world" });
 });
+app.listen(3000);
